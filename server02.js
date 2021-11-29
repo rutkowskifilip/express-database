7a2973d8d9b1cd994823f9d3b9ccfcec60d224d1
rutkowskifilip <95183693+rutkowskifilip@users.noreply.github.com>
2021-11-29 07:55:29 +0100

    Update server02.js

--- server02.js
+++ server02.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 var hbs = require("express-handlebars");
 var path = require("path");
 const Datastore = require("nedb");
